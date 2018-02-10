3155b7e7e7feea007190d564dcbe65d59539e891
Tiffany Wu <tiffwu88@gmail.com>
2018-02-10 17:30:05 -0500

    Add working code for 3rd fn

--- Assignment04.py
+++ Assignment04.py
@@ -1,22 +1,29 @@
 # This is our first assignment as a group
+import numpy as np
 
 
-
-def return_sum()
-
-
-
-
+def main():
+    input_list = [2, -7, 1.5]
+    max_val = return_max_difference(input_list)
+    print('Max diff = {} '.format(max_val))
 
 
-
-def return_min_max()
-
-
+def return_sum():
+    pass
 
 
+def return_min_max():
+    pass
 
 
+def return_max_difference(input_list):
+    input_list = np.array(input_list)
+    diffs = abs(np.diff(input_list))
+    max_val = max(diffs)
+
+    return max_val
 
 
-def return_max_difference()
+if __name__ == "__main__":
+    main()
+
--- test_Assignment04.py
+++ test_Assignment04.py
@@ -1,1 +1,9 @@
 # this is our unit testing for assignment 04
+
+def test_max_diff():
+    from Assignment04 import return_max_difference
+    test_input_list = [[10, 8, 5, 17, 16], [2, -7, 1.5]]
+    test_output_value = [12, 9]
+    for n, t in enumerate(test_input_list):
+        test_output = return_max_difference(t)
+        assert test_output == test_output_value[n]
