a1c552621c0333aca5721be50b92b91395f2b9de
Petek Sener <peteksener9@gmail.com>
2018-02-11 17:15:26 -0500

    Function1

--- Assignment04.py
+++ Assignment04.py
@@ -1,22 +1,25 @@
 # This is our first assignment as a group
 
+def main():
+#   return_sum()
+   max_min = return_min_max()
+#   return_max_difference()
 
 
-def return_sum()
+#def return_sum():
 
 
-
-
-
-
-
-def return_min_max()
-
-
-
+def return_min_max(my_list):
+   max_min = ((max(my_list), min(my_list)))
+   return max_min
 
 
 
 
 
-def return_max_difference()
+#def return_max_difference():
+
+
+
+if __name__ == "__main__":
+   main()
