aed5c8df74ae4dcc4504dada826ffab7cbeb1671
Tiffany Wu <tiffwu88@gmail.com>
2018-02-15 12:29:58 -0500

    Update working code and test code

--- Assignment04.py
+++ Assignment04.py
@@ -1,38 +1,39 @@
 # This is our first assignment as a group
 import numpy as np
 
+
 def main():
-   return_sum(my_list)
-   return_min_max(my_list)
-   return_max_difference(my_list)
+    return_sum(my_list)
+    return_min_max(my_list)
+    return_max_difference(my_list)
 
     
 def return_min_max(my_list):
-   max_min = ((max(my_list), min(my_list)))
-   return max_min
+    max_min = ((max(my_list), min(my_list)))
+    return max_min
     
   
 def return_sum(my_list):
-   sum = 0
-   for elem in my_list:
-      sum += elem
-   return sum
+    sum = 0
+    for elem in my_list:
+        sum += elem
+    return sum
 
 
 def return_max_difference(input_list):
-   try:
-      input_list = np.array(input_list)
-      diffs = abs(np.diff(input_list))
-      max_val = max(diffs)
+    try:
+        input_list = np.array(input_list)
+        diffs = abs(np.diff(input_list))
+        max_val = max(diffs)
 
-      return max_val
-   except ImportError:
-      print('Not valid module')
-   except TypeError:
-      print('Input is not a list')
-   except ValueError:
-      pass
+        return max_val
+    except ImportError:  # redundancy
+        print('Check if numpy is in virtualenv')
+    except TypeError:
+        print('Input is not a list')
+    except ValueError:
+        print('Add more numbers to the list')
 
 
 if __name__ == "__main__":
-   main()
+    main()
--- test_Assignment04.py
+++ test_Assignment04.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+
 def test_sum():
     from Assignment04 import return_sum
     test1 = return_sum([1,3,5,7,9])
@@ -20,14 +21,15 @@ def test_sum():
     
 # Second function
 def test_return_min_max():
-   from Assignment04 import return_min_max
-   test_input_list = ([-1,5,8,100],[5,-8,9,45,88,34,65],[-5,8.234,-99023,342,9.452])
-   test_output_values = ((100,-1),(88,-8),(342,-99023))
+    from Assignment04 import return_min_max
+    test_input_list = ([-1,5,8,100],[5,-8,9,45,88,34,65],[-5,8.234,-99023,342,9.452])
+    test_output_values = ((100,-1),(88,-8),(342,-99023))
 
-   for count, elem in enumerate(test_input_list):   #enuerate itirate and gives the index
-      min_max_output = return_min_max(elem)
-      assert min_max_output == test_output_values[count]
-      assert isinstance(min_max_output, tuple) == True 
+    for count, elem in enumerate(test_input_list):   #enuerate itirate and gives the index
+        min_max_output = return_min_max(elem)
+        assert min_max_output == test_output_values[count]
+        assert isinstance(min_max_output, tuple) == True
+
 
 def test_max_diff():
     from Assignment04 import return_max_difference
@@ -36,7 +38,16 @@ def test_max_diff():
     for n, t in enumerate(test_input_list):
         test_output = return_max_difference(t)
         assert test_output == test_output_value[n]
-    with pytest.raises(TypeError):
+
+    with pytest.raises(ImportError):  # Test ImportError?
+        import scipy
+    with pytest.raises(TypeError):  # Type error when None inputted
         return_max_difference()
+        return_max_difference(['sing'])
+    with pytest.raises(ValueError):  # ValueError can occur when only 1 number given
+        max([])  # This is where it fails in return_max_difference
+
+
+
 
 
