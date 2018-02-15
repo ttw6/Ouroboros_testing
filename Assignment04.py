85b4ef0862f9b326cf82635bf4d1c4ba5bb74122
Tiffany Wu <tiffwu88@gmail.com>
2018-02-15 12:45:03 -0500

    Added docstring

--- Assignment04.py
+++ Assignment04.py
@@ -21,6 +21,18 @@ def return_sum(my_list):
 
 
 def return_max_difference(input_list):
+    """Function will return maximum difference between adjacent numbers.
+
+    Function takes in the inputted list of values, splits it into two arrays to calculate the difference between
+    adjacent values, takes the absolute values of the differences to disregard positioning, and then outputs the
+    maximum value.
+
+    :param input_list: List of numbers
+    :return: Maximum difference
+    :raises ImportError: Check if numpy is installed or virtual env is established
+    :raises TypeError: Input not given as a list of values
+    :raises ValueError: Can occur when only 1 number is given in the list
+    """
     try:
         input_list = np.array(input_list)
         diffs = abs(np.diff(input_list))
@@ -30,7 +42,7 @@ def return_max_difference(input_list):
     except ImportError:  # redundancy
         print('Check if numpy is in virtualenv')
     except TypeError:
-        print('Input is not a list')
+        print('Check if input is a list of values')
     except ValueError:
         print('Add more numbers to the list')
 
