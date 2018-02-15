f2525d1b6f97b38e911d9c6d215ba2e3e002e39b
Tiffany Wu <tiffwu88@gmail.com>
2018-02-15 14:04:51 -0500

    Updated code to include logging and add log file

--- Assignment04.py
+++ Assignment04.py
@@ -1,8 +1,10 @@
 # This is our first assignment as a group
 import numpy as np
+import logging
 
 
 def main():
+    my_list = [2, -7, 1.5]  # Example
     return_sum(my_list)
     return_min_max(my_list)
     return_max_difference(my_list)
@@ -33,18 +35,35 @@ def return_max_difference(input_list):
     :raises TypeError: Input not given as a list of values
     :raises ValueError: Can occur when only 1 number is given in the list
     """
+    # Setup log
+    logging.basicConfig(filename='fn3log.txt', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
+    # Function
     try:
+        logging.info('Start fn')
+        if any(input_list) < 0:  #included to use warning
+            logging.warning('Negative values in list')
+            logging.debug('Values {}'.format(input_list))
         input_list = np.array(input_list)
         diffs = abs(np.diff(input_list))
         max_val = max(diffs)
 
+        logging.info('Max val: {}'.format(max_val))
         return max_val
+
     except ImportError:  # redundancy
-        print('Check if numpy is in virtualenv')
+        logging.debug('Values {}'.format(input_list))
+        logging.error('ImportError: Check if numpy is in virtualenv')
+        #print('Check if numpy is in virtualenv')
     except TypeError:
-        print('Check if input is a list of values')
+        logging.debug('Values {}'.format(input_list))
+        logging.debug(input_list)
+        logging.error('TypeError: Check if input is a list of values')
+        #print('Check if input is a list of values')
     except ValueError:
-        print('Add more numbers to the list')
+        logging.debug('Values {}'.format(input_list))
+        logging.error('ValueError: Add more numbers to the list')
+        #print('Add more numbers to the list')
 
 
 if __name__ == "__main__":
