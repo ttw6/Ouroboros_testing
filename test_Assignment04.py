d85662fca27d231e37cafeac8806688782827701
Petek Sener <peteksener9@gmail.com>
2018-02-15 23:02:57 -0500

    test_Assignment04

--- test_Assignment04.py
+++ test_Assignment04.py
@@ -20,22 +20,25 @@ def test_sum():
     
 # Second function
 def test_return_min_max():
-   from Assignment04 import return_min_max
-   test_input_list = ([-1,5,8,100],[5,-8,9,45,88,34,65],[-5,8.234,-99023,342,9.452])
-   test_output_values = ((100,-1),(88,-8),(342,-99023))
+    from Assignment04 import return_min_max
+    test_input_list = ([-1,5,8,100],[5,-8,9,45,88,34,65],[-5,8.234,-99023,342,9.452])
+    test_output_values = ((100,-1),(88,-8),(342,-99023))
 
-   for count, elem in enumerate(test_input_list):   #enuerate itirate and gives the index
-      min_max_output = return_min_max(elem)
-      assert min_max_output == test_output_values[count]
-      assert isinstance(min_max_output, tuple) == True 
+    for count, elem in enumerate(test_input_list):   #enuerate itirate and gives the index
+        min_max_output = return_min_max(elem)
+        assert min_max_output == test_output_values[count]
+        assert isinstance(min_max_output, tuple) == True 
 
-def test_max_diff():
-    from Assignment04 import return_max_difference
-    test_input_list = [[10, 8, 5, 17, 16], [2, -7, 1.5]]
-    test_output_value = [12, 9]
-    for n, t in enumerate(test_input_list):
-        test_output = return_max_difference(t)
-        assert test_output == test_output_value[n]
+def test_return_min_max_exceptions():
+    from Assignment04 import return_min_max
+    with pytest.raises(TypeError):
+        return_min_max(['string','why'])
+    with pytest.raises(ValueError):
+        return_min_max([])
+
+
+def test_max_diff(): 
+    pass
 
 
 
