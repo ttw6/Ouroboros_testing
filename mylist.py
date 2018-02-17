0a814b92ab8db009bdef6e0d9d11d449a62b0e1b
Tiffany Wu <tiffwu88@gmail.com>
2018-02-17 18:44:18 -0500

    Add class MyList and test file for class

---
+++ mylist.py
@@ -0,0 +1,21 @@
+class MyList:
+
+    def __init__(self, numbers=[1, 2, 3]):
+        self.numbers = numbers
+        self.output_sum = None
+        self.output_min_max = None
+        self.output_max_diff = None
+        self.return_sum()
+        self.return_min_max()
+        self.return_max_diff()
+
+    def return_sum(self):
+        pass
+
+    def return_min_max(self):
+        pass
+
+    def return_max_diff(self):
+        pass
+
+
---
+++ test_mylist.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+def test_sum():
+    pass
+
+
+def test_min_max():
+    pass
+
+
+def test_max_diff():
+    pass
+
